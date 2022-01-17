bb487fb799a8397d65f984875185110064834e96
Madisyn Vail <virtualmadi3@gmail.com>
2022-01-17 12:57:44 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+
+function isWin(element){
+    return element.result === 'W';
+}
+
+function superbowlWin(array) {
+    const Obj = array.find(isWin);
+    if(Obj){
+        return Obj.year;
+    }
+}
